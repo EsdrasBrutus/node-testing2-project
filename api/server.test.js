a43501b48f6b7b3aa77b4dc6e354d1f22bbd4697
GenHero <51133244+GenHero@users.noreply.github.com>
2021-09-09 21:15:41 -0400

    added tests

--- api/server.test.js
+++ api/server.test.js
@@ -25,11 +25,39 @@ describe("endpoints", ()=>{
             expect(res.status).toBe(200)
         })
         it("returns the right format", async ()=>{
-            await db("students").insert(frodo)
-            await db("students").insert(sam)
+            await db("students").insert(student_1)
+            await db("students").insert(student_2)
             const res = await request(server).get("/students")
-            expect(res.body[0]).toMatchObject({id:1,...frodo})
-            expect(res.body[1]).toMatchObject({id:2,...sam})
+            expect(res.body[0]).toMatchObject({id:1,...student_1})
+            expect(res.body[1]).toMatchObject({id:2,...student_2})
+        })
+    })
+    describe("[POST] /students", ()=>{
+        it("responds with newly created student", async ()=>{
+            let res
+            res = await request(server).post("/students").send(student_1)
+            expect(res.body).toMatchObject({id:1, ...student_1})
+        })
+        it("responds with 201 ok", async ()=>{
+            const res = await request(server).post("/students").send(student_2)
+            expect(res.status).toBe(201)
+        })
+    })
+    describe("[DELETE] /students/:id", ()=>{
+        it("responds with 200", async ()=>{
+            await db("students").insert(student_2)
+            const res = await request(server).delete("/students/1")
+            expect(res.status).toBe(200)
+        })
+        it("returns the right num of students", async ()=>{
+            let res
+            await db("students").insert(student_1)
+            await db("students").insert(student_2)
+            res = await request(server).get("/students")
+            expect(res.body).toHaveLength(2)
+
+            res = await request(server).delete("/students/1")
+            expect(res.body).toHaveLength(1)
         })
     })
 })
